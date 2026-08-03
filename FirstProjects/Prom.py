0d1ef9c4a6825e9b949d5d1b66082d8410de5f32
gfrye605-lang <gfrye605@evit.edu>
2026-08-03 13:53:39 -0700

    Beautified code

--- FirstProjects/Prom.py
+++ FirstProjects/Prom.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import time
 
+# -----------------------------------------------
+
 # Question & Answer List
 questions = {"What was the biggest promise I made to you":
              "To be here with you forever",
@@ -9,20 +11,25 @@ questions = {"What was the biggest promise I made to you":
              "How long have we been dating for": "1 year, 3 months",
              "When do couples usually give promise rings":
              "6 months"}
+
+# ------------------------------------------------
+
+# Vars
 question_keys = list(questions.keys())
 current_index = 0
 currentq = question_keys[current_index]
 currentans = questions[currentq]
+
+# ------------------------------------------------
 
 # Answer Logic
 def text_submit():
     global currentans, currentq
     print(currentq,"and ", currentans) # debug
     global current_index
-    print(current_index)
-
+    print(current_index) # debug
     usertxt = entry.get()
-    print(usertxt) # Debug
+    print(usertxt) # debug
     entry.delete(0, tk.END)
     if usertxt == currentans:
         current_index = current_index+1
@@ -32,6 +39,8 @@ def text_submit():
             label.configure(text=currentq, fg="Black")
     elif current_index > 3:
          surpise()     
+
+# ------------------------------------------------
 
 # Special Suprise
 def surpise():
@@ -44,24 +53,32 @@ def surpise():
      question.pack (pady=10)
      question2.pack (pady=10)
 
+# ------------------------------------------------
+
+# Initialize GUI
+
 # 1. Initialize the main application window
 root = tk.Tk()
 root.title("Prom")
 root.geometry("450x250")
 root.configure(background="SlateBlue2")
 
+# ------------------------------------------------
+
 # 2. Create a basic GUI
 label = tk.Label(root, text=question_keys[current_index], font=("Arial", 16))
-
 entry = tk.Entry(root)
 entry.configure(width=27, font=("Arial", 20))
-
 button = tk.Button(root, text="Submit!", font=("Arial", 16), command=text_submit)
+
+# ------------------------------------------------
 
 # 3. Place the parts onto the window
 label.pack(pady=20)
 entry.pack(pady=15)
 button.pack(pady=15)
+
+# -----------------------------------------------
 
 # 4. Start the event loop to keep the window open
 root.mainloop()
